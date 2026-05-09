2e599ed64c164dba9994def660c25145ae110c8a
Andi Gjika <andi.gjika@outlook.com>
2026-05-09 16:10:17 +0200

    Initial commit

---
+++ CryptoAnalytics/src/main/java/andi/cryptoanalytics/CryptoAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package andi.cryptoanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoAnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoAnalyticsApplication.class, args);
+    }
+
+}
---
+++ CryptoAnalytics/src/test/java/andi/cryptoanalytics/CryptoAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package andi.cryptoanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoAnalyticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
